69455e00151428cf0fbc810b13476e9acb59121d
Juanjo <jujoragu@gmail.com>
2021-09-10 15:31:53 -0500

    Add missing line, fix typo

--- asdfInstall.sh
+++ asdfInstall.sh
@@ -2,11 +2,12 @@
 
 sudo apt update
 echo "<--Installing apt modules--->"
-sudo apt install -Y curl git wget
+sudo apt install -y curl git wget
 echo "<---Installing asdf--->"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 echo "" >> ~/.bashrc
 echo "source \$HOME/.asdf/asdf.sh" >> ~/.bashrc
+echo "source \$HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
 source ~/.bashrc
 echo "<---asdf installed---->"
 asdf --version
