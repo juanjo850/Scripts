f1f4cbe6390f1696874e5c9b1901b433d5484893
Juanjo <jujoragu@gmail.com>
2021-09-13 20:20:19 -0500

    bunch more dependencies for erlang

--- asdfInstall.sh
+++ asdfInstall.sh
@@ -3,7 +3,7 @@
 echo "<--Updating apt modules--->"
 sudo apt update
 echo "<--Installing apt modules--->"
-sudo apt install -y curl git wget unzip gcc
+sudo apt install -y curl git wget unzip libssl-dev make automake autoconf libncurses5-dev gcc
 echo "<---Installing asdf--->"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 echo "" >> ~/.bashrc
